103bb923d9e1dc670929eae4954d8118b00ee97b
Kevin Chong <kchong30@my.bcit.ca>
2023-08-20 20:34:43 -0700

    layout for skills section of bio complete

--- src/biography/index.jsx
+++ src/biography/index.jsx
@@ -1,48 +1,25 @@
 import './styles/biography.css'
+import Skills from './skills'
 
 
 function Biography() {
   return (
     <div className="bio-wrapper">
+      <div className={`intro ${scroll < window.innerHeight ? 'fade-in' : ''}`}>
       <div className="title">
-        <h1>About Me<span>.</span></h1>
+        <h1>About Me<span className = "punctuation">.</span></h1>
       </div>
       <div className="animated-line"></div>      
-      <div className="bio-content">
-        <div className="paragraph-one">
-          <p>I am a developer with a deep passion all things web development. I specialize in accessibility, performance, and stability - all while taking into account
-            aesthetic and the end user experience. I strive for bespoke and custom made solutions. There is a time and place for template designs
-            and code - but I truly believe the best content from the web is borne of careful collaboration, communication, and creativity.
+      <div className="paragraph-one">
+          <p className = "mb-3">I'm a developer with a deep passion all things web development.</p> 
+          <p className = "mb-3">I specialize in accessibility, performance, and stability - without sacrificing
+            aesthetic and the end user experience. </p>
+          <p className = "mb-3">Bespoke, custom, tailored products and solutions are what drive me as a professional.
           </p>
         </div>
-        <div className="paragraph-two">
-          <p>Yeah</p>
-        </div>
-      <div className="language-section">
-          <h3>Languages</h3>
-          <div className="grid">
-            <div><i className="fa fa-bar-chart" aria-hidden="true"></i>JavaScript</div>
-            <div><i className="fas fa-python"></i> Python</div>
-            {/* Add more languages */}
-          </div>
-        </div>
-        <div className="framework-section">
-          <h3>Frameworks</h3>
-          <div className="grid">
-            <div><i className="fas fa-react"></i> React</div>
-            <div><i className="fab fa-angular"></i> Angular</div>
-            {/* Add more frameworks */}
-          </div>
-        </div>
-        <div className="database-section">
-          <h3>Databases</h3>
-          <div className="grid">
-            <div><i className="fas fa-database"></i> MySQL</div>
-            <div><i className="fas fa-database"></i> MongoDB</div>
-            {/* Add more databases */}
-          </div>
-        </div>
     </div>
+    <Skills />
+
     </div>
   );
 }
---
+++ src/biography/skills/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import './styles/skills.css'
+
+function Skills() {
+  return (
+    <div className={`skills-section ${scroll >= window.innerHeight * 3 ? 'fade-in' : ''}`}>
+    <h1 className = "skills-title">Skills and Proficiencies<span className = "punctuation">.</span></h1>
+    <div className="animated-line"></div>      
+    <div className="skills-row">
+        <h3>Languages</h3>
+        <div className="badge-grid">
+          <div className="badge-icon"><img src="../../public/logos/js.png" alt="JavaScript"></img>JavaScript</div>
+          <div className="badge-icon"><img src="../../public/logos/html5.png" alt="HTML 5"></img>HTML 5</div>
+          <div className="badge-icon"><img src="../../public/logos/csharp.png" alt="C#"></img>C#</div>
+          <div className="badge-icon"><img src="../../public/logos/python.png" alt="Python"></img>Python</div>
+          <div className="badge-icon"><img src="../../public/logos/css.png" alt="CSS"></img>CSS</div>
+        </div>
+      </div>
+      <div className="skills-row mt-6">
+        <h3>Frameworks</h3>
+        <div className="badge-grid">
+          <div className="badge-icon"><img src="../../public/logos/react.png" alt="React"></img> React</div>
+          <div className="badge-icon"><img src="../../public/logos/angular.png" alt="Angular"></img> Angular</div>
+          <div className="badge-icon"><img src="../../public/logos/vue.png" alt="Vue"></img> Vue</div>
+          <div className="badge-icon"><img src="../../public/logos/tailwind.png" alt="Tailwind"></img> Tailwind</div>
+          <div className="badge-icon"><img src="../../public/logos/bootstrap.png" alt="Bootstrap"></img> Bootstrap</div>
+          <div className="badge-icon"><img src="../../public/logos/netcore.png" alt="ASP.NET"></img> ASP.NET</div>
+          <div className="badge-icon"><img src="../../public/logos/laravel.png" alt="Laravel"></img> Laravel</div>
+          <div className="badge-icon"><img src="../../public/logos/sst.png" alt="SST"></img>SST</div>
+        </div>
+      </div>
+      <div className="skills-row mt-6">
+        <h3>Databases</h3>
+        <div className="badge-grid">
+          <div className="badge-icon"><img src="../../public/logos/mysql.png" alt="MySQL"></img> MySQL</div>
+          <div className="badge-icon-mongo"><img src="../../public/logos/mongodb.png" alt="MongoDB"></img> MongoDB</div>
+          <div className="badge-icon"><img src="../../public/logos/sqlite.png" alt="SQLite"></img> SQLite</div>
+          <div className="badge-icon"><img src="../../public/logos/cockroachdb.png" alt="CockroachDB"></img> CockroachDB</div>
+        </div>
+      </div>
+      <div className="skills-row ">
+        <h3>Cloud Services</h3>
+        <div className="badge-grid">
+          <div className="badge-icon"><img src="../../public/logos/aws.png" alt="AWS"></img> AWS</div>
+          <div className="badge-icon"><img src="../../public/logos/azure.png" alt="Azure"></img> Azure</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills
