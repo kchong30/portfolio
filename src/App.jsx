231be9e36a0d6decb055a4bb966a3e21fbc88724
Kevin Chong <kchong30@my.bcit.ca>
2023-08-24 04:17:20 -0700

    details modification

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,40 @@ import ProjectDetails from './projects/details'
 import useScrollToTop from '../hooks/useScrollToTop'; // Import the custom hook
 
 
-  const projects = [
-        {
-          id:"1",
-          title: "Agilitek - Segmentation Tool",
-          description: "Industry Project sponsored by Agilitek.",
-          technology: ["React", "Node.js", "Express", "MongoDB"],
-          image: "https://i.imgur.com/rOFmbDx.png"
-        },
-        {
-          id:"2",
-          title: "Weather App",
-          description: "Developed a weather application that provides real-time weather information.",
-          technology: ["React", "Node.js", "Express", "OpenWeatherMap API"],
-          image: "https://i.imgur.com/IraHkP4.png"
-        },
-        {
-          id:"3",
-          title: "Task Management System",
-          description: "Created a task management system with user authentication and role-based access control.",
-          technology: ["React", "Node.js", "Express", "MongoDB", "Passport.js"],
-        },
-        // Add more project objects as needed
-      ];
+const projects = [
+  {
+    id: "1",
+    title: "Agilitek - Segmentation Tool",
+    description: "Industry Project sponsored by Agilitek.",
+    overview: "Detailed overview of the project...",
+    challenges: "Challenges faced during the project...",
+    technology: ["React", "Node.js", "Express", "MongoDB"],
+    image: "https://i.imgur.com/rOFmbDx.png",
+    githubLink: "https://github.com/agilitek-repo",
+    deploymentLink: "https://agilitek.com",
+  },
+  {
+    id: "2",
+    title: "Weather App",
+    description: "Developed a weather application that provides real-time weather information.",
+    overview: "Overview of the weather application...",
+    challenges: "Challenges faced during development...",
+    technology: ["React", "Node.js", "Express", "OpenWeatherMap API"],
+    image: "https://i.imgur.com/IraHkP4.png",
+    githubLink: "https://github.com/weather-app-repo",
+  },
+  {
+    id: "3",
+    title: "Task Management System",
+    description: "Created a task management system with user authentication and role-based access control.",
+    overview: "Overview of the task management system...",
+    challenges: "Challenges faced during task management system development...",
+    technology: ["React", "Node.js", "Express", "MongoDB", "Passport.js"],
+    githubLink: "https://github.com/task-management-repo",
+  },
+  // Add more project objects as needed
+];
+
 
 
 
--- src/projects/details/index.jsx
+++ src/projects/details/index.jsx
@@ -4,12 +4,7 @@ import './styles/details.css'
 
 const ProjectDetails = ({ projects }) => {
   const { projectId } = useParams();
-
-  console.log ({projectId})
-  // eslint-disable-next-line react/prop-types
   const project = projects.find(p => p.id === projectId);
-
-  console.log ("hi" + project)
 
   if (!project) return <div>Project not found</div>;
 
@@ -21,10 +16,18 @@ const ProjectDetails = ({ projects }) => {
       <div className="project-info">
         <h2>{project.title}</h2>
         <p>{project.description}</p>
-        <p>Stack: {project.technology.join(', ')}</p>
+        <h3>Overview</h3>
+        <p>{project.overview}</p>
+        <h3>Challenges</h3>
+        <p>{project.challenges}</p>
+        <h3>Tech Stack</h3>
+        <p>{project.technology.join(', ')}</p>
+        <a href={project.githubLink} className="project-link">Github</a>
+        {project.deploymentLink && <a href={project.deploymentLink} className="project-link">Deployment</a>}
       </div>
     </div>
   );
 };
+
 
 export default ProjectDetails;
